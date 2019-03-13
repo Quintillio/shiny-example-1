08c29fc39a1bb58e381df325e6e7c485537ce5c3
ncurcio1 <ncurcio1@drew.edu>
2019-03-12 20:00:09 -0400

    ahhh

--- server.R
+++ server.R
@@ -1,27 +1,42 @@
 library(shiny)
+library(rhandsontable)
 
-# example from https://shiny.rstudio.com/gallery/kmeans-example.html
+# Creating dataset
+# X = as.numeric(rep(NA, times = 10))
+#Y = as.numeric(rep(NA, times =  10))
+X <- c(0,1,2,5,20,80,100,300,500,1000)
+Y <- c(0,0.0154,0.02894,0.04534,0.07138,0.08778,0.08875,0.10032,0.0955,0.06752)
+df1 = data.frame(X=X, Y=Y)
 
-palette(c("#E41A1C", "#377EB8", "#4DAF4A", "#984EA3", "#FF7F00", "#FFFF33", "#A65628", "#F781BF", "#999999"))
 
-# Define server logic required to generate and plot a random distribution
-shinyServer(function(input, output, session) {
-  
-  # Combine the selected variables into a new data frame
-  selectedData <- reactive({
-    iris[, c(input$xcol, input$ycol)]
+shinyServer(function(input,output,session){
+  # returns rhandsontable type object - editable excel type grid data
+  observeEvent(input$plotBtn, {
+    df1 <- hot_to_r(input$table)
+    MMcurve<-formula(df1$Y ~ Vmax* df1$X /(Km + df1$X))
+    bestfit <- nls(MMcurve, df1, start=list(Vmax=0.0035,Km=0.15))
+    Coeffs <- coef(bestfit)
+    output$plot1 <- renderPlot({
+      plot(df1$X,df1$Y)
+      # curve(Coeffs[1]*x/(Coeffs[2]+x), add=TRUE)
+    })
   })
-  
-  clusters <- reactive({
-    kmeans(selectedData(), input$clusters)
+  observeEvent(input$entr,{
+    X = as.numeric(rep(NA, times = input$obs))
+    Y = as.numeric(rep(NA, times = input$obs))
+    #df1 = data.frame(X=X, Y=Y)
+    output$table <- renderRHandsontable({
+      rhandsontable(df1) # converts the R dataframe to rhandsontable object
+    })})
+  observeEvent(input$curveBtn, {
+    df1 <- hot_to_r(input$table)
+    MMcurve<-formula(df1$Y ~ Vmax* df1$X /(Km + df1$X))
+    bestfit <- nls(MMcurve, df1, start=list(Vmax=0.0035,Km=0.15))
+    Coeffs <- coef(bestfit)
+    output$plot1 <- renderPlot({
+      plot(df1$X,df1$Y, pch = 20, cex = 1.5) #pch makes points solid. cex increases their size.
+      curve(Coeffs[1]*x/(Coeffs[2]+x), add=TRUE)
+    })
   })
-  
-  output$plot1 <- renderPlot({
-    par(mar = c(5.1, 4.1, 0, 1))
-    plot(selectedData(),
-         col = clusters()$cluster,
-         pch = 20, cex = 3)
-    points(clusters()$centers, pch = 4, cex = 4, lwd = 4)
-  })
-  
 })
+
